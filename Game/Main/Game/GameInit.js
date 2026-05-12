0dbbcb9b934f11d44844c1190a52c661a6ef615f
AquaPhoenix07 <aquaphoenix040707@gmail.com>
2026-05-13 00:28:48 +0700

    Sửa tạm phần Game

--- Game/Main/Game/GameInit.js
+++ Game/Main/Game/GameInit.js
@@ -1,29 +1,3 @@
-<!DOCTYPE html>
-<html lang="en-us">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Unity Web Player | EcoGuardian-Unity</title>
-    <link rel="shortcut icon" href="TemplateData/favicon.ico">
-    <link rel="stylesheet" href="TemplateData/style.css">
-  </head>
-  <body>
-    <div id="unity-container" class="unity-desktop">
-      <canvas id="unity-canvas" width=960 height=600 tabindex="-1"></canvas>
-      <div id="unity-loading-bar">
-        <div id="unity-logo"></div>
-        <div id="unity-progress-bar-empty">
-          <div id="unity-progress-bar-full"></div>
-        </div>
-      </div>
-      <div id="unity-warning"> </div>
-      <div id="unity-footer">
-        <div id="unity-logo-title-footer"></div>
-        <div id="unity-fullscreen-button"></div>
-        <div id="unity-build-title">EcoGuardian-Unity</div>
-      </div>
-    </div>
-    <script>
       var canvas = document.querySelector("#unity-canvas");
 
       // Shows a temporary message banner/ribbon for a few seconds, or
@@ -51,7 +25,7 @@
         updateBannerVisibility();
       }
 
-      var buildUrl = "Build";
+      var buildUrl = "../../Build";
       var loaderUrl = buildUrl + "/Game.loader.js";
       var config = {
         arguments: [],
@@ -127,7 +101,3 @@
             };
 
       document.body.appendChild(script);
-
-    </script>
-  </body>
-</html>
